d9b997944dbb0483d0585780d55aa8cebabf92f5
LemonWang2022 <j090290@my.cmsh.cyc.edu.tw>
2022-11-30 18:51:45 +0800

    suck

--- src/main/java/frc/robot/name/Ning.java
+++ src/main/java/frc/robot/name/Ning.java
@@ -1,6 +1,11 @@
 package frc.robot.name;
 
-public class Ning {
+import edu.wpi.first.wpilibj.XboxController;
+import com.ctre.phoenix.motorcontrol.can.WPI_VictorSPX;
+
+public class Robot extends TimedRobot {
+    WPI_VictorSPX suck = new WPI_VictorSPX(17);
+    XboxController xbox = new XboxController(0);{
     if(xbox.getRawButton(5)){ 
         suck.set(0.5);  
       } else{
