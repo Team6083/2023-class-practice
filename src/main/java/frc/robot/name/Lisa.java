98f6826f630635a93107e1a50018b63b873313d2
Lisacyj0102 <s1110169@my.cmsh.cyc.edu.tw>
2022-11-30 18:31:43 +0800

    shoot

--- src/main/java/frc/robot/name/Lisa.java
+++ src/main/java/frc/robot/name/Lisa.java
@@ -1,5 +1,13 @@
 package frc.robot.name;
 
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+
 public class Lisa {
-    
+    CANSparkMax spark1 = new CANSparkMax(21, MotorType.kBrushless);
+    if(xbox.getYButton()){
+        spark1.set(0.3);
+      }else{
+        spark1.set(0);
+      }
 }
